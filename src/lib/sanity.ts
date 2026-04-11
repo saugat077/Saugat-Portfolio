4916bc85e5d9a06250111c9b89f1459098b769d5
Saugat K.C <ksaugat77@gmail.com>
2026-04-11 21:58:40 +0545

    Feat: Astro setup with tailwind, Sanity Client and Working data Fetch

---
+++ src/lib/sanity.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = createClient({
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  useCdn: true,
+  apiVersion: '2024-01-01',
+  token: import.meta.env.SANITY_READ_TOKEN,
+})
+
+const builder = imageUrlBuilder(client)
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function urlFor(source: any) {
+  return builder.image(source)
+}
